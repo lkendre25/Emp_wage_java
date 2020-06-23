be5489bf04c9754becef86a370e17883dd86754e
lkendre25 <lkendre5652@gmail.com>
2020-06-23 16:21:08 +0530

    commited UC-2

--- Test.java
+++ Test.java
@@ -1,19 +1,19 @@
 import java.util.Random;
-class Test{
-
+class test{
         public static void main(String args[]){
                Random rand=new Random();
                int rand_int1 = rand.nextInt(2);
                if(rand_int1==1){
-               System.out.println("Present !! ");
+                  System.out.println("Present !! ");
+                  int WAGE_PER_HOUR=20;
+                  int HOUR=8;
+                  int total_sal;
+                  total_sal=WAGE_PER_HOUR*HOUR;
+                  System.out.println("Total Salary of full day is : "+total_sal);
                }
                else{
-
                   System.out.println("Absent !! ");
                }
-
         }
-
-
 }
 
