a587e0beba0fdcfd7d4b96009cad4774da39e58d
lkendre25 <lkendre5652@gmail.com>
2020-06-23 16:29:33 +0530

    Commited

--- demoFile.java
+++ demoFile.java
@@ -6,15 +6,20 @@ class test{
                if(rand_int1==1){
                   System.out.println("Present !! ");
                   int WAGE_PER_HOUR=20;
-                  int HOUR=8;
+                  int FULLDAY=8;
+                  int HALFDAY=4;
+                  if(FULLDAY==8){
                   int total_sal;
-                  total_sal=WAGE_PER_HOUR*HOUR;
+                  total_sal=WAGE_PER_HOUR*FULLDAY;
                   System.out.println("Total Salary of full day is : "+total_sal);
+                  }else{
+                  int total_sal=WAGE_PER_HOUR*HALFDAY;
+                  System.out.println("Half day salary is : "+total_sal);
+                  }
                }
                else{
                   System.out.println("Absent !! ");
                }
         }
 }
-
 
