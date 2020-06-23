ba13845c14f5ec8537faf8bd7920d71dd06c633a
lkendre25 <lkendre5652@gmail.com>
2020-06-23 16:32:36 +0530

    Commited

--- demoFile.java
+++ demoFile.java
@@ -1,25 +1,28 @@
 import java.util.Random;
 class test{
         public static void main(String args[]){
-               Random rand=new Random();
-               int rand_int1 = rand.nextInt(2);
-               if(rand_int1==1){
-                  System.out.println("Present !! ");
-                  int WAGE_PER_HOUR=20;
-                  int FULLDAY=8;
-                  int HALFDAY=4;
-                  if(FULLDAY==8){
+            Random rand=new Random();
+            int rand_int1 = rand.nextInt(3);
+            int WAGE_PER_HOUR=20;
+            int FULLDAY=8;
+            int HALFDAY=4;
+            switch (rand_int1){
+               case 1:
                   int total_sal;
                   total_sal=WAGE_PER_HOUR*FULLDAY;
                   System.out.println("Total Salary of full day is : "+total_sal);
-                  }else{
-                  int total_sal=WAGE_PER_HOUR*HALFDAY;
+               break;
+               case 2:
+                  System.out.println("Half Day!! ");
+                  total_sal=WAGE_PER_HOUR*HALFDAY;
                   System.out.println("Half day salary is : "+total_sal);
-                  }
-               }
-               else{
-                  System.out.println("Absent !! ");
-               }
-        }
+               break;
+               case 0:
+                  System.out.println("Absent!! ");
+               break;
+               default:
+                  System.out.println("Invalid Input");
+            }
+   }
 }
 
